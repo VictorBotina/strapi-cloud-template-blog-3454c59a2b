f3aa2156401ef88478edf4e0493811f65dd0c588
Victor Botina <victorbotina@emssanareps.co>
2025-10-28 14:36:38 -0500

    initi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -611,6 +611,7 @@ export interface ApiHomeHome extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    feedback: Schema.Attribute.Component<'shared.feedback-card', true>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::home.home'> &
       Schema.Attribute.Private;
